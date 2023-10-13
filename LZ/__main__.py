3af21cfc97190e483d3d1f4b7536746fcdc23ef0
AgustinRatner <agustinratsk8@gmail.com>
2023-10-13 17:46:23 -0300

    Solucione un error  lógico que tenia que ver con el orden del codigo

--- LZ/__main__.py
+++ LZ/__main__.py
@@ -1,17 +1,15 @@
 import math
 import re
 
-# Supongamos que el tamaño máximo de ventana es 16, entonces requiere de 4 bits; Tomaremos una de 4
-VENTANA = 2
+# Supongamos que el tamaño máximo de ventana es 16, entonces requiere de 4 bits;
+VENTANA = 4
 VENTANA_MAX_BITS = 4
 def lecturaCadena():
     archivo = open("../datos/texto1.txt", 'r')
     cadena = archivo.readline()
     patron = r'[-,;._\s+]'
 
-    # Vamos a tomar como delimitadores que separan subcadenas a los "espacios,_,-,punto y coma,punto")
-    # limpiamos la cadena
-
+    # limpiamos la cadena de "espacios,_,-,punto y coma,punto"
     cadena_limpia = re.sub(patron, "", cadena)
     return cadena_limpia
 
@@ -24,8 +22,6 @@ def capturar_simbolos(cadena):
 
 def codigo_bloque(simbolos, bits_codigo):
     pseudo = list()
-    # Aca le decimos que no tome "0b0" sino que omita 0b y quede con "0"
-    # Además también queremos que el codigo sea de cierta longitud (000 por ej)
     pseudo.append(bin(0)[2:].zfill(bits_codigo))
 
     for i in range(1,len(simbolos)):
@@ -38,7 +34,7 @@ def codigo_bloque(simbolos, bits_codigo):
 def match(cadena_ventana,cadena_no_comprimida):
     i=len(cadena_no_comprimida)
     cadena2=''.join(cadena_ventana)
-    while i > 0:
+    while i >= 0:
         cadena1=cadena_no_comprimida[:i]
         if cadena2.find(cadena1) != -1:
             return cadena1
@@ -54,15 +50,18 @@ def imprimirTabulaciones(ventana_lectura,contador):
 def comprimir(cadena,pseudo_ascii,simbolos):
     ventana_lectura=[''] * VENTANA
 
-    # Primero codificamos en 4 bits el valor "2" que es el tamaño de la ventana, porque el receptor debe saber
-    # cual es el tamaño de ventana para luego decodificar
+    # Primero codificamos en 4 bits el valor "n < 16" que es el tamaño de la ventana, para que el receptor
+    # luego pueda decodificar
     comprimido = bin(VENTANA)[2:].zfill(VENTANA_MAX_BITS)
 
-    # Llenamos la ventana y codificamos como si fueran coincidencias (Ya se encuentran los primeros 2 en la ventana)
+    # Llenamos la ventana y codificamos solo con el codigo de pseudo (porque inicialmente la ventana esta llena)
+    # Luego en la lectura sabremos n° de bits iniciales que solo expresan los codigos de los "si"
+    # cargados en la ventana, porque ya sabemos con los primeros bits el tamaño de la ventana, siendo un total
+    # "log2(VENTANA)"
     if len(ventana_lectura) < len(cadena):
-        for i in range(len(ventana_lectura)):
+        for i in range(VENTANA):
             ventana_lectura[i]=cadena[i]
-            simb = '1' + pseudo_ascii[simbolos.index(cadena[i])]
+            simb = pseudo_ascii[simbolos.index(cadena[i])]
             comprimido = comprimido + simb
     else:
         return None
@@ -70,7 +69,6 @@ def comprimir(cadena,pseudo_ascii,simbolos):
     print("\nTamaño de ventana + primeros \"n\" simbolos de la ventana: " + comprimido+"\n")
 
     si=len(ventana_lectura)
-
     contador=0
     while si < len(cadena):
         imprimirTabulaciones(ventana_lectura,contador)
@@ -81,59 +79,28 @@ def comprimir(cadena,pseudo_ascii,simbolos):
         print("cadena que hizo match: " + str(cadena_match))
 
         if len(cadena_match) == 0 or len(cadena_match) == 1:
-            for i in range(1,len(ventana_lectura)):
-                ventana_lectura[i-1]=ventana_lectura[i]
-
-            ventana_lectura[len(ventana_lectura)-1]=cadena[si]
-
             if len(cadena_match) == 0:
                 # flag = 1 + codPseudo = (1 + long_codigo) bits
                 simb = '1' + pseudo_ascii[simbolos.index(cadena[si])]
-
             else:
                 simb = '0' + bin(ventana_lectura.index(cadena[si]))[2:].zfill(int(math.log2(VENTANA))) + bin(1)[2:].zfill(int(VENTANA))
 
+            for i in range(1,VENTANA):
+                ventana_lectura[i-1]=ventana_lectura[i]
+
+            ventana_lectura[VENTANA-1]=cadena[si]
             comprimido = comprimido + simb
             si += 1
-
         else:
-            for i in range(len(cadena_match)):
-                ventana_lectura[i] = ventana_lectura[VENTANA - len(cadena_match) +i]
-                ventana_lectura[VENTANA - len(cadena_match) + i] = cadena[si+i]
-
-
-            # flag = 0 + posicion_coincidencia = log2(ventana) + long_coincidencia (Como maximo es "tamaño de ventana")
+            # flag = 0 + posicion_coincidencia expresada en log2(ventana) bits + long_coincidencia expresada en log2(ventana) bits
             simb = '0' + bin(ventana_lectura.index(cadena[si]))[2:].zfill(int(math.log2(VENTANA))) + bin(len(cadena_match))[2:].zfill(int(VENTANA))
             comprimido = comprimido + simb
+
+            for i in range(len(cadena_match)):
+                ventana_lectura[i] = ventana_lectura[VENTANA - len(cadena_match)+i]
+                ventana_lectura[VENTANA - len(cadena_match) + i] = cadena[si+i]
+
             si += len(cadena_match)
-
-            """
-            long_coincidencia = math.log2(len(cadena_match))
-            if long_coincidencia % 2 == 0:
-                simb = '0' + bin(ventana_lectura.index(cadena[si]))[2:].zfill(int(math.log2(VENTANA))) + bin(len(cadena_match))[2:].zfill(int(long_coincidencia))
-            else:
-                simb = '0' + bin(ventana_lectura.index(cadena[si]))[2:].zfill(int(math.log2(VENTANA))) + bin(len(cadena_match))[2:].zfill(int(long_coincidencia + 1))
-            """
-
-        """ if cadena[i] not in ventana_lectura:
-            for i in range(1,len(ventana_lectura)):
-                ventana_lectura[i-1]=ventana_lectura[i]
-
-            ventana_lectura[len(ventana_lectura)-1]=cadena[i]
-
-            # flag = 1 + codPseudo = (1 + long_codigo) bits
-            simb = '1' + pseudo_ascii[simbolos.index(cadena[i])]
-            comprimido = comprimido + simb
-        else:
-            for i in range(1,len(ventana_lectura)):
-                ventana_lectura[i-1]=ventana_lectura[i]
-
-            ventana_lectura[len(ventana_lectura)-1]=cadena[i]
-
-            # flag = 0 + posicion_coincidencia = log2(ventana) + long_coincidencia = 1 bit (En este caso)
-            simb = '0' + bin(ventana_lectura.index(cadena[i]))[2:].zfill(int (math.log2(VENTANA))) + '1'
-            comprimido = comprimido + simb
-            """
 
     imprimirTabulaciones(ventana_lectura,contador+1) # Ultima impresion
     return comprimido
